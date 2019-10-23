ecfea5cead3dbc9e8d43324afe1e27f839dc0c1f
andersbuck <buckles.anderson@gmail.com>
2019-10-22 22:20:48 -0400

    Fix sonar issues

--- angular-springboot/web-app/client/src/app/service/user.service.ts
+++ angular-springboot/web-app/client/src/app/service/user.service.ts
@@ -9,36 +9,18 @@ import { User } from '../model/user';
 })
 export class UserService {
 
-  private user: User = <User>{};
+  private user: User = {} as User;
   private userUrl = 'http://localhost:8080/web-app/user';
   private errorUrl = 'http://localhost:8080/web-app/causeerror';
 
   constructor(private http: HttpClient) { }
 
   getUser(): Observable<User> {
-    return this.http.get<User>(this.userUrl).pipe(
-      catchError(this.handleError<User>('getUser', <User>{}))
-    );
+    return this.http.get<User>(this.userUrl);
   }
 
   getError(): Observable<any> {
     return this.http.get<any>(this.errorUrl);
   }
-
-  /**
- * Handle Http operation that failed.
- * Let the app continue.
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
-private handleError<T> (operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
-
-    // TODO: send the error to remote logging infrastructure
-    console.error('Hey this is my error!' + error); // log to console instead
-
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
 }
-}
+
